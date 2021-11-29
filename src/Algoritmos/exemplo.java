d5ce13fbc791651f44c934e09145d663829a5387
Guilherme Portella <59876059+GuilhermePortella@users.noreply.github.com>
2021-11-28 23:41:11 -0300

    Update exemplo.java

--- src/Algoritmos/exemplo.java
+++ src/Algoritmos/exemplo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Algoritmos;
 
 import java.io.BufferedWriter;
@@ -19,10 +14,10 @@ public class exemplo {
         
         BufferedWriter br = new BufferedWriter(new FileWriter("file2"));
         
-        br.write("Vamos escrever nesse novo arquivo em Java! que legal hahaha!!!");
+        br.write("Vamos escrever nesse novo arquivo em Java!");
         br.newLine();
         br.newLine();
-        br.write("Vamos escrever outra linha aqui embaixo hahaha!!!");
+        br.write("Vamos escrever outra linha");
         br.close();
     }
 
