5941fbfaf22e438d582e28462188893b3da0b810
guilhermeportella <guilhermeportella2@gmail.com>
2021-12-15 23:01:15 -0300

    Ex Cap 5

--- src/Capitulo_5/MinMax.java
+++ src/Capitulo_5/MinMax.java
@@ -13,24 +13,26 @@ public class MinMax {
         int nums[] = new int[80];
         int min, max;
 
-//        nums[0] = 99;
-//        nums[1] = 199;
-//        nums[2] = 90;
-//        nums[3] = -219;
-//        nums[4] = 81992;
-//        nums[5] = 1211;
-//        nums[6] = -991;
-//        nums[7] = 15;
-//        nums[8] = 9;
-//        nums[9] = 1;
+        nums[0] = 99;
+        nums[1] = 199;
+        nums[2] = 90;
+        nums[3] = -219;
+        nums[4] = 81992;
+        nums[5] = 1211;
+        nums[6] = -991;
+        nums[7] = 15;
+        nums[8] = 9;
+        nums[9] = 1;
 
-        Random rd = new Random();
-        for (int i = 0; i < 80; i++) {
-            nums[i] = rd.nextInt();
-        }
-        for (int i = 0; i < 80; i++) {
-            System.out.println(nums[i]);
-        }
+        
+        //numeros aleatorios para o array//
+//        Random rd = new Random();
+//        for (int i = 0; i < 80; i++) {
+//            nums[i] = rd.nextInt();
+//        }
+//        for (int i = 0; i < 80; i++) {
+//            System.out.println(nums[i]);
+//        }
         
         min = max = nums[0];
         
