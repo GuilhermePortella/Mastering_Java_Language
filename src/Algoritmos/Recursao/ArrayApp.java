7a0363bb412efe8e5d356c4e82355933e4b82edb
guilhermeportella <guilhermeportella2@gmail.com>
2022-03-06 23:11:09 -0300

    Novos algotimos grafos e recursividade

--- src/Algoritmos/Recursao/ArrayApp.java
+++ src/Algoritmos/Recursao/ArrayApp.java
@@ -5,15 +5,15 @@ package Algoritmos.Recursao;
  * @author Guilherme
  */
 public class ArrayApp {
-    
+
     public static void main(String[] args) {
-        
+
         long[] arr;                 //referencia vetor
         arr = new long[100];        //cria vetor
         int nElems = 0;             //numero de itens
         int j;                      //contador do laço
         long searchKey;             //chave do item a pesquisar
-        
+
         arr[0] = 77;                //inserir 10 itens
         arr[1] = 99;
         arr[2] = 44;
@@ -25,11 +25,41 @@ public class ArrayApp {
         arr[8] = 66;
         arr[9] = 33;
         nElems = 10;                //agora 10 itens no vetor
-        
+
         for (j = 0; j < nElems; j++) {
             System.out.print(arr[j] + " ");
         }
         System.out.println("");
+
+        searchKey = 66;
+        for (j = 0; j < nElems; j++) {
+            if (arr[j] == searchKey) {
+                break;
+            }
+        }
+        if (j == nElems) {
+            System.out.print("Can't find " + searchKey);
+        } else {
+            System.out.println("Found " + searchKey);
+        }
+
+        
+        searchKey = 55;
+        
+        for (j = 0; j < nElems; j++) {
+            if (arr[j] == searchKey) {
+                break;
+            }
+        }
+        for (int k = j; k < nElems; k++) {
+            arr[k] = arr[k+1];
+        }
+        nElems --;
+
+        for (j = 0; j < nElems; j++) {
+            System.out.print(arr[j]+ " ");
+        }
+        System.out.println("");
     }
-    
+
 }
