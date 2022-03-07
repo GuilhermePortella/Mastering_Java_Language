cf69d2ee02b092ef8a7f33981f19c6579522b86f
guilhermeportella <guilhermeportella2@gmail.com>
2022-03-06 23:11:09 -0300

    Novos algotimos grafos e recursividade

--- src/Algoritmos/Grafos/DFSApp.java
+++ src/Algoritmos/Grafos/DFSApp.java
@@ -9,21 +9,20 @@ public class DFSApp {
     public static void main(String[] args) {
         
         Graph theGraph = new Graph();
-        theGraph.addVertex('A');
-        theGraph.addVertex('B');
-        theGraph.addVertex('C');
-        theGraph.addVertex('D');
-        theGraph.addVertex('E');
+        theGraph.addVertex('A'); // 1
+        theGraph.addVertex('B'); // 2
+        theGraph.addVertex('C'); // 3
+        theGraph.addVertex('D'); // 4
+        theGraph.addVertex('E'); // 5
         
-        theGraph.addEdge(0, 1);
-        theGraph.addEdge(1, 2);
-        theGraph.addEdge(0, 3);
-        theGraph.addEdge(3, 4);
+        theGraph.addEdge(0, 1); //A B
+        theGraph.addEdge(1, 2); //B C
+        theGraph.addEdge(0, 3); //A D
+        theGraph.addEdge(3, 4); //D E
         
-        System.out.println("Visits: ");
+        System.out.print("Visits: ");
         theGraph.dfs();
         System.out.println();
-        
     }
     
 }
