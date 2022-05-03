a9b94d78d6b445beaac240ddb4967fb8ef207a83
guilhermeportella <guilhermeportella2@gmail.com>
2022-05-03 00:45:05 -0300

    Excecao nao tratada

---
+++ src/Capitulo_9/ExcTest.java
@@ -0,0 +1,32 @@
+
+package Capitulo_9;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class ExcTest {
+    //gerando uma excecao
+    
+    static void genException() {
+        int nums[] = new int[4];
+        
+        System.out.println("Before exception is generated.");
+        
+        //gera uma excecao de indice fora do limite
+        nums[7] = 10;
+        System.out.println("this won1't be displayed");
+    }
+}
+
+class ExcDemo2 {
+    public static void main(String[] args) {
+        try {
+            ExcTest.genException();
+        }catch(ArrayIndexOutOfBoundsException exc){
+            //captura excecao
+            System.out.println("Index out-of-bounds!");
+        }
+        System.out.println("After catch statement");
+    }
+}
