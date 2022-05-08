be570985509961f17dc59aa3e26920755be74c49
guilhermeportella <guilhermeportella2@gmail.com>
2022-05-07 22:47:24 -0300

    Ordenacao com shell - fanalizacao

--- src/Algoritmos/Ordenacao/ShellSort.java
+++ src/Algoritmos/Ordenacao/ShellSort.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Algoritmos.Ordenacao;
 
 /**
@@ -10,5 +5,63 @@ package Algoritmos.Ordenacao;
  * @author Guilherme
  */
 public class ShellSort {
+    public static void main(String[] args) {
+        int maxSize = 10;
+        ArraySh arr;
+        arr = new ArraySh(maxSize);
+        
+        for (int j = 0; j < maxSize; j++) {
+            long n = (int)(java.lang.Math.random()*99);
+            arr.insert(n);
+        }
+        arr.display();
+        arr.shellSort();
+        arr.display();
+    }
+}
+
+class ArraySh{
+    private long[ ] theArray;
+    private int nElems;
     
+    public ArraySh(int max){
+        theArray = new long[max];
+        nElems = 0;
+    }
+    
+    public void insert(long value){
+        theArray[nElems] = value;
+        nElems++;
+    }
+    
+    public void display(){
+        System.out.print("A=");
+        for (int j = 0; j < nElems; j++) {
+            System.out.print(theArray[j] + " ");
+        }
+        System.out.println(" ");
+    }
+    
+    public void shellSort(){
+        int inner, outer;
+        long temp;
+        
+        int h = 1;
+        while (h <= nElems/3) {
+            h = h*3 + 1;
+        }
+        while (h>0) {
+            for (outer = h; outer<nElems; outer++) {
+                temp = theArray[outer];
+                inner = outer;
+                
+                while (inner > h-1 && theArray[inner-h] >= temp) {
+                    theArray[inner] = theArray[inner-h];
+                    inner -= h;   
+                }
+                theArray[inner] = temp;
+            }
+            h = (h-1)/3;
+        }
+    }
 }
