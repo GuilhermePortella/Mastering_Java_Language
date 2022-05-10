2752ec474f7ca92f371f8f52ebf2432289dc9dff
guilhermeportella <guilhermeportella2@gmail.com>
2022-05-10 00:24:41 -0300

    Tipos genericos

---
+++ src/Capitulo__13/Gen.java
@@ -0,0 +1,43 @@
+package Capitulo__13;
+
+/**
+ *
+ * @author Guilherme
+ */
+class Gen<T> {
+
+    T ob;
+
+    Gen(T o) {
+        ob = o;
+    }
+    
+    T getob(){
+        return ob;
+    }
+    
+    void showType() {
+        System.out.println("Type of T is " + ob.getClass().getName());
+    }
+}
+
+class GenDemo {
+    public static void main(String[] args) {
+        Gen<Integer> iOb;
+        iOb = new Gen<Integer>(88);
+        
+        iOb.showType();
+        
+        int v = iOb.getob();
+        System.out.println("value " + v);
+        
+        System.out.println();
+        
+        Gen<String> strOb = new Gen<String>("Generics Test");
+        strOb.showType();
+        
+        String str = strOb.getob();
+        System.out.println("value " + str);
+    }
+    
+}
