fff82e05bd142c24879c07390ce517be6df502c0
guilhermeportella <guilhermeportella2@gmail.com>
2022-06-20 23:55:20 -0300

    Estudando cod do stack

---
+++ src/Algoritmos/Logica/Converter_para_Hexadecimal.java
@@ -0,0 +1,33 @@
+package Algoritmos.Logica;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Converter_para_Hexadecimal {
+      private static final int sizeOfIntInHalfBytes = 8;
+  private static final int numberOfBitsInAHalfByte = 4;
+  private static final int halfByte = 0x0F;
+  private static final char[] hexDigits = { 
+    '0', '1', '2', '3', '4', '5', '6', '7', 
+    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+  };
+
+  public static String decToHex(int dec) {
+    StringBuilder hexBuilder = new StringBuilder(sizeOfIntInHalfBytes);
+    hexBuilder.setLength(sizeOfIntInHalfBytes);
+    for (int i = sizeOfIntInHalfBytes - 1; i >= 0; --i)
+    {
+      int j = dec & halfByte;
+      hexBuilder.setCharAt(i, hexDigits[j]);
+      dec >>= numberOfBitsInAHalfByte;
+    }
+    return hexBuilder.toString(); 
+  }
+
+  public static void main(String[] args) {
+     int dec = 10;
+     String hex = decToHex(dec);
+     System.out.println(hex);       
+  }
+}
