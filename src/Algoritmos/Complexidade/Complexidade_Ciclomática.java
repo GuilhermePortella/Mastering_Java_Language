f5b3b083fe6f1389be3af0d01246c33e9233fb5e
guilhermeportella <guilhermeportella2@gmail.com>
2022-08-06 21:28:20 -0300

    Complexidade_Ciclomática

---
+++ src/Algoritmos/Complexidade/Complexidade_Ciclomática.java
@@ -0,0 +1,29 @@
+package Algoritmos.Complexidade;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Complexidade_Ciclomática {
+
+//    void receive(Object object) {
+//        if (object instanceof ObjectTypeA) {
+//            doSomethingA();
+//        } else {
+//            if (object instanceof ObjectTypeB) {
+//                doSomethingB();
+//            } else {
+//                if (object instanceof ObjectTypeC) {
+//                    doSomethingC();
+//                } else {
+//                    if (object instanceof ObjectTypeD) {
+//                        doSomethingD();
+//                    } else {
+//                        // etc...
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+}
