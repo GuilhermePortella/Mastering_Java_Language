e7cb7f30f98a65914000b4434fe444009f7c5cac
GuilhermePortella <guilhermeportella2@gmail.com>
2022-10-12 19:42:11 -0300

    Adicionando diretorio para o livro deitel

---
+++ src/bookDeitel/init.java
@@ -0,0 +1,12 @@
+package bookDeitel;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class init {
+    public static void main(String[] args) {
+        System.out.println("ola mundo");
+    }
+    
+}
