93d9ce0ddff0712b8345aaef34dc0ff75795502d
GuilhermePortella <guilhermeportella2@gmail.com>
2026-02-17 19:31:12 -0300

    Capitulo_1_Fundamentos do livro do schildt

---
+++ src/main/java/books/schildt/Capitulo_1_Fundamentos/BlocoDemo.java
@@ -0,0 +1,20 @@
+package books.schildt.Capitulo_1_Fundamentos;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class BlocoDemo {
+    public static void main(String args[]){
+        double i, j, d;
+        i = 5;
+        j = 10;
+        
+        // O alvo desta instrução if é um bloco Codigo
+        if(i != 0){
+        System.out.println("I does not equal zero"); // ---|
+        d = j/i;                                     // O alvo de If é este bloco inteiro.
+        System.out.println("j / i is " + d);         //----|
+    }
+    }
+}
---
+++ src/main/java/books/schildt/Capitulo_1_Fundamentos/IfDemo.java
@@ -0,0 +1,35 @@
+package books.schildt.Capitulo_1_Fundamentos;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class IfDemo {
+    public static void main(String args[]){
+        int a, b, c;
+        
+        a = 2;
+        b = 3;
+        
+        if(a < b) System.out.println("A is less than b");
+        //não exibe nada
+        
+        if(a == b) System.out.println("You won's see this");
+        
+        System.out.println();
+        
+        c = a - b; //C contem -1
+        
+        System.out.println("C contains - 1");
+        if(c >= 0) System.out.println("C is non-negative");
+        if(c < 0) System.out.println("C is negative");
+        
+        System.out.println();
+        
+        c = b - a;//agora C contem 1 
+        
+        System.out.println("C contains 1");
+        if(c >= 0) System.out.println("C is non-negative");
+        if(c <0) System.out.println("C is negative");
+    }
+}
